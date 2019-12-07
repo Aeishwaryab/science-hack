b060ab303ab795e1476d0c1d08b343daf74963f6
garimagarg14 <58623038+garimagarg14@users.noreply.github.com>
2019-12-07 20:50:59 +0100

    Update temp_co2_matlab.m

--- temp_co2_matlab.m
+++ temp_co2_matlab.m
@@ -1,22 +1,16 @@
 data.world.kaya = xlsread("ChallengeData.xlsx",1,'C6:AU6')
-data.ref.ei = xlsread("ChallengeData.xlsx",4,'U6')
-data.ref.ci = xlsread("ChallengeData.xlsx",5,'U6')
-co2he_ref = data.ref.ei*data.ref.ci/1E8
-co2he = data.world.kaya*co2he_ref
-% 
-% kayapast.world = xlsread("ChallengeData.xlsx",1,'C6:AU6')
-% 
-% fig_kaya = figure
-% hold on
-% plot (kaya.world)
-% plot(kayapast.world)
-
-co2ea = co2he*0.4
+%ref = 1990
+data.ref.ei = xlsread("ChallengeData.xlsx",4,'U6') % TPES (PJ)E15
+data.ref.ci = xlsread("ChallengeData.xlsx",5,'U6') % CO2/TPES (Tonnes/TJ)E12
+co2he_ref = data.ref.ei*data.ref.ci/1E6 %Gigatonnes
+co2he = (data.world.kaya*co2he_ref)/100 %world data (from 1971 to 2015)Step 1
+co2ea = co2he*0.5 %Step 2; Airborne Factor of 0.4
 plot(co2ea)
 co2ppm = co2ea/7.81
 plot(co2ppm)
 for i=2:45
-    co2ppm(i)=co2ppm(i-1)*exp(-1/500)+co2ppm(i)
+    co2ppm(i)=co2ppm(i-1)*exp(-1/500)+co2ppm(i) %step 3 time constant = 500
 end
 plot (co2ppm)
-temp = 14.2+2*log2((co2ppm+326)/326)
+temp = 14.2+4*log2((co2ppm+326)/326)
+plot(temp)
