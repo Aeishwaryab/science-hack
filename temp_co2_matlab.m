088e0a728b41c490920fd5d03360eea67e56fdf9
Dthushara246 <58608604+Dthushara246@users.noreply.github.com>
2019-12-08 10:40:36 +0100

    Add

--- temp_co2_matlab.m
+++ temp_co2_matlab.m
@@ -3,15 +3,7 @@ data.ref.ei = xlsread("ChallengeData.xlsx",4,'U6')
 data.ref.ci = xlsread("ChallengeData.xlsx",5,'U6')
 co2he_ref = data.ref.ei*data.ref.ci/1E8
 
-
 co2he = data.world.kaya*co2he_ref
-% 
-% kayapast.world = xlsread("ChallengeData.xlsx",1,'C6:AU6')
-% 
-% fig_kaya = figure
-% hold on
-% plot (kaya.world)
-% plot(kayapast.world)
 
 co2ea = co2he*0.4
 plot(co2ea)
@@ -23,4 +15,3 @@ for i=2:45
 end
 plot (co2ppm)
 temp = 14.2+2*log2((co2ppm+326)/326)
-tempx = temp'
